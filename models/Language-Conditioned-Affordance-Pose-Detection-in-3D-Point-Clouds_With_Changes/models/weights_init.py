e376258e1bf9876e3f81c43d926fbde434d6df30
mfarhadhussain <farhadh@iisc.ac.in>
2025-03-16 17:27:29 +0530

    adding models dir and submodules with changes and keeping it local

---
+++ models/Language-Conditioned-Affordance-Pose-Detection-in-3D-Point-Clouds_With_Changes/models/weights_init.py
@@ -0,0 +1,15 @@
+import torch
+
+def weights_init(m):
+    """_summary_
+    Weights initialization
+    """
+    classname = m.__class__.__name__
+    if classname.find('Conv2d') != -1:
+        torch.nn.init.xavier_normal_(m.weight.data)
+        if m.state_dict().get('bias') != None:
+            torch.nn.init.constant_(m.bias.data, 0.0)
+    elif classname.find('Linear') != -1:
+        torch.nn.init.xavier_normal_(m.weight.data)
+        if m.state_dict().get('bias') != None:
+            torch.nn.init.constant_(m.bias.data, 0.0)
